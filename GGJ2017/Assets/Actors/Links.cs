78ca231b05f0885d0a7fafd4acea865c8f161d46
westin <westin007@gmail.com>
2017-01-21 09:43:37 -0600

    Fixed - Mr.Black

--- GGJ2017/Assets/Actors/Links.cs
+++ GGJ2017/Assets/Actors/Links.cs
@@ -7,18 +7,30 @@ public class Links : MonoBehaviour
 {
     public GameObject personA;
     public GameObject personB;
-
     public bool visible;
 
-    public GameObject drawLinkPiece;
-    public float LinkWidth = .5f;
-    public float LinkSpacing = .02f;
 
-    private int lastnum = 0;
+    public GameObject drawLinkPiece;
+    public float baseScale = 1f;
 
-    // Use this for initialization
+    public int numPieces = 10;
+
+
+    private float LinkWidth = .5f;
+
+
+    // Use this for initialization
     void Start()
     {
+        for (int i = 0; i < numPieces; i++)
+        {
+            var temp = GameObject.Instantiate(drawLinkPiece);
+
+            temp.transform.position = transform.position;
+            temp.transform.localPosition = new Vector3(LinkWidth, 0) * i;
+            temp.transform.parent = this.gameObject.transform;
+
+        }
     }
 
 
@@ -26,29 +38,15 @@ public class Links : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var placea = personA.transform.position;
-        var placeb = personB.transform.position;
+        var placea = personA.transform.position;        var placeb = personB.transform.position;
         this.transform.position = placea;
 
-        var dist = placeb - placea;
-        var num = Mathf.FloorToInt( Mathf.Abs( dist.magnitude/(LinkWidth)));
-
-        if (lastnum != num)        {            List<GameObject> children = new List<GameObject>();
-            for (int i = 0; i < this.transform.childCount; i++)
-            {
-                children.Add(transform.GetChild(i).gameObject);
-            }
-            children.ForEach(Destroy);
-
-            for (int i = 0; i < num; i++)            {                var temp = GameObject.Instantiate(drawLinkPiece);
-                temp.transform.position = transform.position;
-                temp.transform.localPosition += new Vector3(LinkWidth + LinkSpacing, 0) * i;                temp.transform.parent = this.gameObject.transform;
-            }
-
-        }
-        lastnum = num;
-
-        
+        var dist = placeb - placea;
+        for (int i = 0; i < transform.childCount; i++)
+        {            var temp = transform.GetChild(i);
+            temp.transform.localPosition = new Vector3(dist.magnitude/numPieces, 0) * i;
+        }
+      
         dist.Normalize();        float rot_z = Mathf.Atan2(dist.y, dist.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(rot_z, Vector3.forward);
 
