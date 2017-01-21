0ad8af2f492bea484375e8604d51a39d1532d1ae
westin <westin007@gmail.com>
2017-01-21 00:11:20 -0600

    I think creating parents resets positioning.... - Mr. Black

---
+++ GGJ2017/Assets/Actors/Links.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Links : MonoBehaviour
+{
+    public GameObject personA;
+    public GameObject personB;
+
+    public bool visible;
+
+    public GameObject drawLinkPiece;
+    public float LinkWidth = .5f;
+    public float LinkSpacing = .02f;
+
+    private int lastnum = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        var placea = personA.transform.position;
+        var placeb = personB.transform.position;
+        this.transform.position = placea;
+
+        var dist = placeb - placea;
+        var num = Mathf.FloorToInt( Mathf.Abs( dist.magnitude/(LinkWidth)));
+
+        if (lastnum != num)        {            List<GameObject> children = new List<GameObject>();
+            for (int i = 0; i < this.transform.childCount; i++)
+            {
+                children.Add(transform.GetChild(i).gameObject);
+            }
+            children.ForEach(Destroy);
+
+            for (int i = 0; i < num; i++)            {                var temp = GameObject.Instantiate(drawLinkPiece);
+                temp.transform.position = transform.position;
+                temp.transform.localPosition += new Vector3(LinkWidth + LinkSpacing, 0) * i;                temp.transform.parent = this.gameObject.transform;
+            }
+
+        }
+        lastnum = num;
+
+        
+        dist.Normalize();        float rot_z = Mathf.Atan2(dist.y, dist.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(rot_z, Vector3.forward);
+
+        Debug.Log(personA.transform.position);
+
+
+
+    }
+}
---
+++ GGJ2017/Assets/Actors/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Person : MonoBehaviour
+{
+
+    public List<GameObject> connections;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
