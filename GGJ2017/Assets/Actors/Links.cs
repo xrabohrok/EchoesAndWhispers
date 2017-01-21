b3f3c5f09f641cad36bd5850d170a8f64e5caab1
westin <westin007@gmail.com>
2017-01-21 11:36:21 -0600

    visibility works - Mr.Black

--- GGJ2017/Assets/Actors/Links.cs
+++ GGJ2017/Assets/Actors/Links.cs
@@ -7,7 +7,7 @@ public class Links : MonoBehaviour
 {
     public Person personA;
     public Person personB;
-    public bool visible;
+    public bool Visible = true;
     public GameObject drawLinkPiece;
     public float baseScale = 1f;
 
@@ -16,7 +16,7 @@ public class Links : MonoBehaviour
 
     private float LinkWidth = .5f;
 
-
+    private bool wasVisible = true;
     // Use this for initialization
     void Start()
     {
@@ -38,26 +38,42 @@ public class Links : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-
-        if (personA != null && personB != null)        {
-            var placea = personA.transform.position;            var placeb = personB.transform.position;
+    {        if (personA != null && personB != null)
+        {
+            var placea = personA.transform.position;
+            var placeb = personB.transform.position;
 
             this.transform.position = placea;
-
             var dist = placeb - placea;
 
+
             for (int i = 0; i < transform.childCount; i++)
+
             {
                 var temp = transform.GetChild(i);
                 temp.transform.localPosition = new Vector3(dist.magnitude/numPieces, 0)*i;
-
             }
-
 
             dist.Normalize();
             float rot_z = Mathf.Atan2(dist.y, dist.x)*Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(rot_z, Vector3.forward);
+
         }
+        if (Visible && !wasVisible)        {
+            for (int i = 0; i < transform.childCount; i++)
+            {                var temp = transform.GetChild(i);
+                temp.gameObject.SetActive(true);            }
+        }
+        if (!Visible && wasVisible)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                var temp = transform.GetChild(i);
+                temp.gameObject.SetActive(false);
+            }
+        }
+
+        wasVisible = Visible;
+
     }
 }
