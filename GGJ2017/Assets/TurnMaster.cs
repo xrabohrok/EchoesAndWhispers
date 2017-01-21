057a2c45b9e82efc227c9b8f84702ff823b93fad
westin <westin007@gmail.com>
2017-01-21 13:26:06 -0600

    basic turn thing - Mr.Black

---
+++ GGJ2017/Assets/TurnMaster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnMaster : MonoBehaviour
+{
+
+    public List<TurnPhase> TurnPhases;
+    public GameObject gameCam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    foreach (var turnPhase in TurnPhases)
+	    {
+	        if (!turnPhase.amIDone())
+	        {
+	            turnPhase.RecieveCameraControl(gameCam.GetComponent<Camera>());
+                turnPhase.DoPhase();
+	        }
+	    }
+	}
+}
+
+public interface TurnPhase
+{
+    void DoPhase();
+    void RecieveCameraControl(Camera cam);
+    bool amIDone();
+
+}
