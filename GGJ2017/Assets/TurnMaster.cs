db71fbe79977f648775abc225457e49e68d2605e
westin <westin007@gmail.com>
2017-01-21 13:59:00 -0600

    on your turn, YOU DO NOTHING - Mr.Black

--- GGJ2017/Assets/TurnMaster.cs
+++ GGJ2017/Assets/TurnMaster.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 public class TurnMaster : MonoBehaviour
 {
 
-    public List<TurnPhase> TurnPhases;
+    public List<GameObject> TurnPhases;
     public GameObject gameCam;
+
+    private List<TurnPhase> turnParts;
 
 	// Use this for initialization
 	void Start () {
-		
+		turnParts = new List<TurnPhase>();
+	    foreach (var turnPhase in TurnPhases)
+	    {
+	        var temp = turnPhase.GetComponent<TurnPhase>();
+	        if (temp != null)
+	        {
+	            turnParts.Add(temp);
+	        }
+	    }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    foreach (var turnPhase in TurnPhases)
+	    foreach (var turnPhase in turnParts)
 	    {
 	        if (!turnPhase.amIDone())
 	        {
