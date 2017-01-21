c3f3a108f62b624302ad478ab09030c0fe4ed169
westin <westin007@gmail.com>
2017-01-21 14:17:35 -0600

    Changed Links to Link as intended - Mr.Black

--- GGJ2017/Assets/Actors/Link.cs
+++ GGJ2017/Assets/Actors/Link.cs
@@ -8,15 +8,16 @@ public class Link : MonoBehaviour
     public Person personA;
     public Person personB;
     public bool Visible = true;
+    public GameObject drawLinkPiece;
+    public float baseScale = 1f;
 
-    public GameObject drawLinkPiece;
+    public int numPieces = 10;
 
-    public float baseScale = 1f;
-    public int numPieces = 10;
-    private float LinkWidth = .5f;
-    private bool wasVisible = true;
 
-    // Use this for initialization
+    private float LinkWidth = .5f;
+
+    private bool wasVisible = true;
+    // Use this for initialization
     void Start()
     {
         for (int i = 0; i < numPieces; i++)
@@ -31,54 +32,48 @@ public class Link : MonoBehaviour
     }
 
     public void recieveTargets(Person a, Person b)
-
-    {
-        personA = a;
+    {        personA = a;
         personB = b;
-    }
+    }
 
     // Update is called once per frame
     void Update()
-    {
+    {        if (personA != null && personB != null)
+        {
+            var placea = personA.transform.position;
+            var placeb = personB.transform.position;
 
-        if (personA != null && personB != null)
-        {
-            var placea = personA.transform.position;
-            var placeb = personB.transform.position;
+            this.transform.position = placea;
+            var dist = placeb - placea;
 
-            this.transform.position = placea;
-            var dist = placeb - placea;
 
+            for (int i = 0; i < transform.childCount; i++)
+
+            {
+                var temp = transform.GetChild(i);
+                temp.transform.localPosition = new Vector3(dist.magnitude/numPieces, 0)*i;
+            }
+
+            dist.Normalize();
+            float rot_z = Mathf.Atan2(dist.y, dist.x)*Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(rot_z, Vector3.forward);
+
+        }
+        if (Visible && !wasVisible)        {
             for (int i = 0; i < transform.childCount; i++)
-            {
-                var temp = transform.GetChild(i);
-                temp.transform.localPosition = new Vector3(dist.magnitude/numPieces, 0)*i;
-            }
-
-
-            dist.Normalize();
-            float rot_z = Mathf.Atan2(dist.y, dist.x)*Mathf.Rad2Deg;
-            transform.rotation = Quaternion.AngleAxis(rot_z, Vector3.forward);
-        }
-
-        if (Visible && !wasVisible)
-        {
-            for (int i = 0; i < transform.childCount; i++)
-            {
-                var temp = transform.GetChild(i);
-                temp.gameObject.SetActive(true);
-            }
+            {                var temp = transform.GetChild(i);
+                temp.gameObject.SetActive(true);            }
         }
         if (!Visible && wasVisible)
-        {
-            for (int i = 0; i < transform.childCount; i++)
-            {
-                var temp = transform.GetChild(i);
-                temp.gameObject.SetActive(false);
-            }
-        }
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                var temp = transform.GetChild(i);
+                temp.gameObject.SetActive(false);
+            }
+        }
 
-        wasVisible = Visible;
+        wasVisible = Visible;
 
-    }
+    }
 }
