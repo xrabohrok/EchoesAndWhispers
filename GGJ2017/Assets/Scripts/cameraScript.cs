cc932d43d8f80488a0b1296a43b23ab2f0b76158
Matthew Wilmsmeyer <mwilmsm@gmail.com>
2017-01-21 11:34:29 -0600

    Ported Westin's click code from game jam 2016. - Mr. Blue

--- GGJ2017/Assets/Scripts/cameraScript.cs
+++ GGJ2017/Assets/Scripts/cameraScript.cs
@@ -1,48 +1,50 @@
-﻿using System.Collections;
+﻿    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameraScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
-
-public void focusPerson(Transform coordinates)
+public class cameraScript : MonoBehaviour
 {
-    //TODO:Move network to person, center left person
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void focusPerson(Transform coordinates)
+    {
+        //TODO:Move network to person, center left person
 
 
-}
+    }
 
-public void showNetwork()
-{
-    //TODO: Zoom out, character on left investigators on right
+    public void showNetwork()
+    {
+        //TODO: Zoom out, character on left investigators on right
 
-}
+    }
 
-public void cameraDrag()
-{
-    //TODO: Pan and move the camera
+    public void cameraDrag()
+    {
+        //TODO: Pan and move the camera
 
 
-}
+    }
 
-public void cameraZoom(object input)
-{
-//TODO: Take in object then zoom in or out
+    public void cameraZoom(object input)
+    {
+        //TODO: Take in object then zoom in or out
 
-}
+    }
 
-public void focusDual(Transform one, Transform two)
-{
-    //TODO: Implement  zooming on two targets
+    public void focusDual(Transform one, Transform two)
+    {
+        //TODO: Implement  zooming on two targets
 
+    }
 }
