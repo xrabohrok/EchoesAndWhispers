a6cc05cbe6b02dfe83c971df420d8515712d85ef
Matthew Wilmsmeyer <mwilmsm@gmail.com>
2017-01-22 11:44:04 -0600

    Added SoundEffectScript. --Mr. Blue

---
+++ GGJ2017/Assets/Scripts/SoundEffectScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectScript : MonoBehaviour {
+
+    public AudioClip gunshot;
+    public AudioClip scissors;
+    public AudioClip murmurs;
+    public AudioClip clearThroat;
+
+    private AudioSource source;
+
+
+	// Use this for initialization
+	void Awake () {
+        this.source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayGunshot()
+    {
+        source.PlayOneShot(this.gunshot, 1.0f);
+    }
+
+    public void PlayScissors()
+    {
+        source.PlayOneShot(this.scissors, 1.0f);
+    }
+
+    public void PlayMurmurs()
+    {
+        source.PlayOneShot(this.murmurs, 1.0f);
+    }
+
+    public void PlayClearThroat()
+    {
+        source.PlayOneShot(this.clearThroat, 1.0f);
+    }
+
+}
