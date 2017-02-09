8359d179c6438733fb07af4b23dad843d074d83c
xrabohrok <westin007@gmail.com>
2017-02-08 21:06:15 -0600

    REALLY basic pip tracker framework - Mr.Black

---
+++ GGJ2017/Assets/PipTracker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipTracker : MonoBehaviour
+{
+
+    public int pipsPerDay;
+
+    private int workingPipsRemaining;
+    public int pipsRemaining
+    {
+        get { return workingPipsRemaining; }
+    }
+
+    public void newTurnStarts()
+    {
+        workingPipsRemaining = pipsPerDay;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+        newTurnStarts();
+	}
+
+    public bool attemptAddPips(int morePips)
+    {
+        if (morePips > workingPipsRemaining)
+            return false;
+
+        workingPipsRemaining -= morePips;
+        return true;
+    }
+
+
+}
