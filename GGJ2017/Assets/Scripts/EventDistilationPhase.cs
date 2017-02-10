a0528ad42a198175fc6891fa53c0843775180243
xrabohrok <westin007@gmail.com>
2017-02-09 21:19:38 -0600

    event notification and basic turn phase handler - Mr.Black

---
+++ GGJ2017/Assets/Scripts/EventDistilationPhase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventDistilationPhase : MonoBehaviour, TurnPhase {
+    private Camera theCam;
+    private TurnMaster myMaster;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+
+    public void turnStart()
+    {
+        //not much to do here
+    }
+
+    public void DoPhase()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void RecieveCameraControl(Camera cam)
+    {
+        theCam = cam;
+    }
+
+    public void informOfRealDad(TurnMaster master)
+    {
+        myMaster = master;
+    }
+
+    public bool amIDone()
+    {
+        throw new System.NotImplementedException();
+    }
+}
