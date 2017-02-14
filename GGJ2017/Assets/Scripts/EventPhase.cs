8d5c8120fa8bdb196e5b31440ce9d377aa1b5e7a
xrabohrok <westin007@gmail.com>
2017-02-13 21:24:25 -0600

    shim event - Mr.Black

---
+++ GGJ2017/Assets/Scripts/EventPhase.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventPhase : MonoBehaviour, TurnPhase {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void turnStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DoPhase()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void RecieveCameraControl(Camera cam)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void informOfRealDad(TurnMaster master)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool amIDone()
+    {
+        throw new System.NotImplementedException();
+    }
+}
